ce3344a5be494791d7718b988d8e5cf4a558973f
tucker mayo <tuckerjamesmayo@gmail.com>
2015-02-02 15:27:30 -0800

    add layout, refactor contact and index

--- app.rb
+++ app.rb
@@ -6,33 +6,28 @@ require('./lib/phone')
 require('./lib/contact')
 
 get('/') do
-  @phone = Phone.all()
+  @contacts = Contact.all()
+  erb(:index)
+end
+
+post('/contacts') do
+  name = params['name']
+  Contact.new({:name => name}).save()
+  @contacts = Contact.all()
   erb(:index)
 end
 
 post('/phone') do
-  name = params.fetch('name')
-  Phone.new(name).save()
-  @phone = Phone.all()
-  erb(:index)
-end
-
-post('/contact') do
-  name = params.fetch('name')
+  type = params.fetch('type')
   number = params.fetch('number')
-  @contact = Contact.new(name, number)
-  @contact.save()
-  @phone = Phone.find(params.fetch('phone_id').to_i())
-  @phone.add_number(@contact)
-  erb(:phone)
-end
-
-get('/contact/:id') do
-  @contact = Contact.find(params.fetch('id'))
+  @number = Phone.new({:type => type, :number => number})
+  @number.save()
+  @contact = Contact.find(params['contact_id'].to_i())
+  @contact.add_number(@number)
   erb(:contact)
 end
 
-get('/phone/:id') do
-  @phone = Phone.find(params.fetch('id').to_i())
-  erb(:phone)
+get('/contacts/:id') do
+  @contact = Contact.find(params['id'].to_i())
+  erb(:contact)
 end
